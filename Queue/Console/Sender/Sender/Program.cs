7ac7dbf9b97b824c56965dbb7c36a723c742127e
Wictor Chaves <wictor@hotmail.com.br>
2023-04-29 09:49:12 -0300

    Enviando mensagem para a function

---
+++ Queue/Console/Sender/Sender/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.Storage.Queues.Models;
+using Sender;
+
+using (var queueManager = QueueManager.CreateInstace("fila-teste"))
+{
+    Console.WriteLine("Press any key to stop...");
+    Console.ReadKey();
+
+    Console.WriteLine($"Fila criada");
+    Console.WriteLine($"Quantidade de mensagens na fila {await queueManager.GetLengthAsync()}");
+
+    var sendReceipt = await queueManager.InsertMessage("Hello, World");
+    Console.WriteLine($"Mensagem adicionada");
+    Console.WriteLine($"Quantidade de mensagens na fila {await queueManager.GetLengthAsync()}");
+
+    sendReceipt = await queueManager.InsertMessage("Hello, World, Again");
+    Console.WriteLine($"Mais uma mensagem adicionada");
+    Console.WriteLine($"Quantidade de mensagens na fila {await queueManager.GetLengthAsync()}");
+
+    Console.WriteLine("Press any key to stop...");
+    Console.ReadKey();
+}
